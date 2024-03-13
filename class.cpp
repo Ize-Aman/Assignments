162e8216e64e6bc49964ac0ed0186d3326504129
Ammanuel Tesfahun <156339946+Ize-Aman@users.noreply.github.com>
2024-03-13 19:48:19 +0300

    Add files via upload
    And this one, you type a letter and it prints out the letter "C" from the letter you typed.
    for example, if you type  in the letter "X" it will make the letter "C" made of many "X"

---
+++ class.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string X = "";
+    cout<< "enter a letter";
+    cin>> X;
+  cout<<"  "<<X << X << X <<endl;  
+  cout<<" "<<X << "   " << X <<endl;        
+  cout<<    X<<endl;
+  cout<<    X<<endl;
+  cout<<    X<<endl;
+  cout<<    X<<endl;
+  cout<<    X<<endl;
+  cout<<" "<<X << "   " << X <<endl;      
+  cout<<"  "<<X << X << X; 
+
+    return 0;
+}
